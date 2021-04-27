17dd90769224dc1ecdfa1f420da5fd8bb7284b36
Jones <jones.omoyibo@decagon.dev>
2021-04-27 20:10:13 +0100

    New changes made

--- app/src/main/java/com/decagon/jones/githubactiondemo/MainActivity.kt
+++ app/src/main/java/com/decagon/jones/githubactiondemo/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Hello World")
+        println("Hope it Works this Time")
     }
 }
